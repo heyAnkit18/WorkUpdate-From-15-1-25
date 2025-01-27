3066206f1098f4115eb4556f73283f62ee423a08
heyAnkit18 <ankitkumar@aaa2innovate.com>
2025-01-27 10:18:25 +0530

    vehicleType Master Added zipaworld

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-    // assoctions folder was created to manage associations and remove circular dependencies
     ...require('./Associations/index'),
     // master folder    
     vehicleTypeMaster:require("./vehicalType/index")
